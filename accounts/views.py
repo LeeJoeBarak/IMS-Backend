94ae166675a31e24874a22aa1ad48a72b0fa1dc5
LeeJoeBarak <ljbarak@gmail.com>
2022-01-04 20:56:07 +0200

    WIP - add student registeration path

--- accounts/views.py
+++ accounts/views.py
@@ -1,17 +1,45 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
+from django.http import FileResponse
+from manage import db, get_collection_handle
+
+import datetime
 
 # Create your views here.
 
 # example for  function that is triggered by route '' (landing page of the website)
 
 ############# Need to implement until Jan 6th, 2022 ##############
-def home(request):
-    return HttpResponse('home')
-
 
 def register_student(request):
-    return NotImplemented
+    if request.method == "POST":
+        username = request.POST.get('username')
+        firstName = request.POST.get('firstName')
+        lastName = request.POST.get('lastName')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+
+        data = {'username': username,
+                'firstName': firstName,
+                'lastName': lastName,
+                'password': password,
+                'email': email}
+        #todo validate that the student is a BGU student
+
+        # create this student in the DB
+        studentCol = db["students"]
+        print(f"retrieved student collection:\n {studentCol}")
+
+        res = studentCol.insert_one(data)
+        print("successful insert")
+
+        print(res.inserted_id)
+
+        return JsonResponse(data, safe=False)
+    return HttpResponse("request method wasn't POST")
+
+
 
 
 def create_internship(request):
@@ -26,6 +54,10 @@ def pick_internships(request):
 
 
 ###################################################################
+def home(request):
+    return HttpResponse('home !!! :)')
+
+
 def login(request):
     return NotImplemented
 
