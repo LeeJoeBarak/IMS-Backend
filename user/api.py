d96c596f3742322e7a2fc0d360151a8bf0eef0d4
yuvalmoryosef <yuvalmoryosef3@gmail.com>
2022-03-05 22:25:12 +0200

    Path POST users/register/student work - fix responses

--- user/api.py
+++ user/api.py
@@ -1,6 +1,7 @@
 from rest_framework import generics, permissions
 from rest_framework.response import Response
 from knox.models import AuthToken
+from rest_framework import status
 
 from .models import Student
 from .serializer import UserSerializer, RegisterSerializer, LoginSerializer
@@ -14,14 +15,22 @@ class RegisterAPI(generics.GenericAPIView):
 
     def post(self, request, *args, **kwargs):
         serializer = self.get_serializer(data=request.data)
-        serializer.is_valid(raise_exception=True)
+        if not serializer.is_valid():
+            return Response('A user with the same username already exists', status.HTTP_400_BAD_REQUEST)
+        # serializer.is_valid(raise_exception=True)
         user = serializer.save()
-        student_user = Student.objects.create(user_id=UserSerializer(user, context=self.get_serializer_context()).data['id'])
+
+        student_user = Student.objects.create(
+            user_id=UserSerializer(user, context=self.get_serializer_context()).data['id'])
         student_user.save()
-        return Response({
-            "user": UserSerializer(user, context=self.get_serializer_context()).data,
-            "token": AuthToken.objects.create(user)[1],
-        })
+        return Response(
+            content_type='A new user has been added',
+            status=status.HTTP_201_CREATED,
+            data={
+                "user": UserSerializer(user, context=self.get_serializer_context()).data,
+                "token": AuthToken.objects.create(user)[1],
+            }
+        )
 
 
 # Login API
