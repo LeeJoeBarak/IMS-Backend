5d54ffbc5e855572d28bada2fd340acfd5a2d83f
yuvalmoryosef <yuvalmoryosef3@gmail.com>
2022-03-13 19:15:43 +0200

    Implement POST /programManager/createInternship

--- internship/serializers.py
+++ internship/serializers.py
@@ -1,4 +1,6 @@
 from rest_framework import serializers
+
+import internship
 from .models import Internship
 
 
@@ -13,12 +15,20 @@ class InternshipsSerializer(serializers.ModelSerializer):
         # "requirments": "string"
 
 
-# class NewInternshipSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = Internship
-#         fields = (
-#         'companyRepresentative', 'program', 'internshipName', 'companyName', 'about', 'requirements', 'mentor')
+class CreateInternshipSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Internship
+        fields = ('program_id', 'internshipName', 'companyName_id', 'about', 'requirements')
 
+        # def create(self, validated_data):
+        #     internship = Internship.objects.create(
+        #         internshipName=validated_data['internshipName'],
+        #         program_id=validated_data['program'],
+        #         companyName_id=validated_data['company'],
+        #         about=validated_data['about'],
+        #         requirements=validated_data['requirements']
+        #     )
+        #     return internship
 
 # "username": "string",
 # "companyName": "string",
