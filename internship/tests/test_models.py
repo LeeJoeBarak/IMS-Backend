d28940e2a89b5f33035d90421a38aedf643dec98
LeeJoeBarak <ljbarak@gmail.com>
2022-05-12 12:24:59 +0300

    add TestInternshipDetails and TestPriority test cases

--- internship/tests/test_models.py
+++ internship/tests/test_models.py
@@ -8,27 +8,20 @@ import pytest
 from rest_framework.test import APITestCase
 from model_bakery import baker
 
-from internship.models import InternshipDetails
-from program.models import Program
-from user.models import Company
+from internship.models import *
+from program.models import *
+from user.models import *
 
 # Fixtures are little pieces of data that serve as the baseline for your tests.
 class TestInternshipDetails(APITestCase):
     def setUp(self):
         self.program = Program.objects.create(program="2022")
-        print(self.program)
-
         self.company = Company.objects.create(companyName="GM")
-        print(self.company)
 
     def test_internship_details_model(self):
-        # programs = Program.objects.all()
-        # program = programs.filter(program='2020')
-        # program = Program.objects.get(program='2020')
-
         internship_details = baker.make(InternshipDetails, internshipName='Embedded intern', program=self.program, companyName=self.company)
         # internship_details = InternshipDetails(internshipName='Embedded intern', program='2020', companyName='GM')
-        internship_details.save()
+        # internship_details.save()
 
         # Check database
         new_internship_details = InternshipDetails.objects.get(internshipName='Embedded intern')
@@ -41,7 +34,25 @@ class TestInternshipDetails(APITestCase):
 
 
 class TestPriority(APITestCase):
-    pass
+    def setUp(self):
+        self.user = User.objects.create_user("test_user", "test_user@test.io", "some_pass")
+        self.student = Student.objects.create(user=self.user)
+
+        self.program = Program.objects.create(program="2022")
+
+        self.company = Company.objects.create(companyName="test_company_1")
+        self.internship_details_1 = baker.make(InternshipDetails, internshipName='test_internship_1', program=self.program, companyName=self.company)
+        self.company = Company.objects.create(companyName="test_company_2")
+        self.internship_details_2 = baker.make(InternshipDetails, internshipName='test_internship_2', program=self.program, companyName=self.company)
+
+    def test_priority_model(self):
+        priority1 = baker.make(Priority, internship=self.internship_details_1, Student=self.student, student_priority_number=1)
+        priority2 = baker.make(Priority, internship=self.internship_details_2, Student=self.student, student_priority_number=2)
+        new_priorities = Priority.objects.filter(Student=self.student)
+
+        # validate count() == 2
+        self.assertEqual(new_priorities.count(), 2)
+
 
 
 class TestAssignmentIntern(APITestCase):
@@ -52,21 +63,18 @@ class TestHoursReport(APITestCase):
     pass
 
 
-class Test(APITestCase):
+class TestInternshipAndMentor(APITestCase):
     pass
 
 
-class Test(APITestCase):
+class TestInternshipAndIntern(APITestCase):
     pass
 
 
-class Test(APITestCase):
+class TestInternReport(APITestCase):
     pass
 
 
-class Test(APITestCase):
+class TestMentorReport(APITestCase):
     pass
 
-
-class Test(APITestCase):
-    pass
